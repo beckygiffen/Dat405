bbee866f3a93347efb267c962986614178572608
becky giffen <rebecca.giffen@students.plymouth.ac.uk>
2018-10-30 17:39:52 +0000

    dhjfl`hfkjdshf

--- loopsssss/sketch.js
+++ loopsssss/sketch.js
@@ -1,18 +1,36 @@
 //Loops
-let size = 30;
+let size1 = 50;
+let size2 = 30;
+
 
 function setup(){
   createCanvas (500,500);
-  noLoop();
+ noLoop();
 
 }
-
 function draw(){
-  fill(215,200,170);
-  for (let y =0; y<10; y++){
-    for (let x = 0; x<10; x++){
-      rect(size*x, size*y, size,size);
-      console.log("x: "+x+", y: "+y);
+
+  for (let y=0; y<10; y++){
+   for (let x =0; x<10; x++){
+     fill("brown");
+      rect(size1*x, size1*y, size1,size1);
     }
-  }ggkhghj
+
+      //small squares
+
+  for (let y=0; y<10; y++){
+    for (let x =0; x<10; x++){
+      fill(random(255),random(255),random(255));
+      rect (size1*x+10, size1*y+10, size2, size2);
+
+
+            //console.log("x: "+x+", y: "+y);
+
+
+
+
+
+    }
+  }
+}
 }
