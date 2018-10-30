204b572dcb3c670167cee9e03c9f6f7b101a1432
becky giffen <rebecca.giffen@students.plymouth.ac.uk>
2018-10-30 15:22:59 +0000

    update

--- loopsssss/sketch.js
+++ loopsssss/sketch.js
@@ -2,16 +2,16 @@
 let size = 50;
 
 function setup(){
-createCanvas (500,500);
-noLoop();
+ createCanvas (500,500);
+ noLoop();
 
-}
+ }
 
-function draw(){
-fill(215,200,170);
-for let(let x = 0; x<10; x++){
-  rect(size*x, height/2, size,size);
-  
+ function draw(){
+ fill(215,200,170);
+ for (let x = 0; x<10; x++){
+ rect(size*x, height/2, size,size);
+
 }
 
 
