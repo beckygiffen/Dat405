4417a9743c27a60ad31502ab8edc9cc61e34af77
becky giffen <rebecca.giffen@students.plymouth.ac.uk>
2018-11-05 11:33:30 +0000

    hgjhg

--- courseworkpractice/sketch.js
+++ courseworkpractice/sketch.js
@@ -1,7 +1,18 @@
-function setup() {
-  fill('black');
-  stroke('white');
-  createCanvas(100,100);
+//Loops
+let size1 = 50;
+let size2 = 30;
 
 
+function setup(){
+  createCanvas (500,500);
+ //noLoop();
+
+}
+function draw(){
+
+  for (let y=0; y<10; y++){
+   for (let x =0; x<10; x++){
+     fill("brown");
+      rect(size1*x, size1*y, size1,size1);//x 0 x50 etc
+    }
 }
