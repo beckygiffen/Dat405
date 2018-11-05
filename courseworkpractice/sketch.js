dad1b19e3a92a0ec503a12644208bc52d028975a
becky giffen <rebecca.giffen@students.plymouth.ac.uk>
2018-11-05 11:38:08 +0000

    c

--- courseworkpractice/sketch.js
+++ courseworkpractice/sketch.js
@@ -1,19 +1,11 @@
 //Loops
-let size1 = 50;
-let size2 = 30;
+//let size1 = 50;
+//let size2 = 30;
 
 
 function setup(){
   createCanvas (500,500);
- noLoop();
+rect(10,10,10,10)
 
-}
-function draw(){
 
-  for (let y=0; y<10; y++){
-   for (let x =0; x<10; x++){
-     fill("white");
-     strokeWeight("1");
-      rect(size1*x, size1*y, size1,size1);//x 0 x50 etc
-    }
 }
