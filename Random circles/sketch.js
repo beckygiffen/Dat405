f9b1d5f1add2ffae369f0c777216f5aa03407240
becky giffen <rebecca.giffen@students.plymouth.ac.uk>
2018-11-06 16:46:37 +0000

    boooo

---
+++ Random circles/sketch.js
@@ -0,0 +1,19 @@
+//RANDOMCIRCLESBABES
+let size = random(500);
+
+function setup() {
+  createCanvas(500, 500);
+  frameRate(20);
+}
+
+function draw() {
+  //Run the nested loop for x, and y
+
+      //Draw the background shapes
+      fill(random(100), random(0), random(255), random(150))
+      noStroke();
+
+      ellipse(random(500), random(500), random(500));
+
+
+    }
