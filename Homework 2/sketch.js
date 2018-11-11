59245e2190e2e6eaf46f97cb7da8c99bc83d39ef
becky giffen <rebecca.giffen@students.plymouth.ac.uk>
2018-11-11 22:26:24 +0000

    awda

--- Homework 2/sketch.js
+++ Homework 2/sketch.js
@@ -1,4 +1,7 @@
 //Create three global scope variables for position and size
+let circlez;
+let squarez;
+let trigl;
 let xRandom;
 let yRandom;
 let sizeRandom = 100;
@@ -8,9 +11,19 @@ function setup() {
   frameRate(10); //Drawing speed 10 fps
   noStroke();
 }
+function keyPressed() {
+  if (key == 'd' || key == 'D') circlez;
+  if (key == 'a' || key == 'A') squarez;
+  if (key == 's' || key == 'S') trigl;
+
+  console.log("x: " +xPlusOne+ ", y: " +yPlusOne);
+}
 
 function draw() {
   //Randomize the variables for the position and size
+  squarez = random(rects);
+  trigl = random(triangle);
+  circlez = random(ellipse);
   xRandom = random (width);
   yRandom = random (height);
   sizeRandom = random(100, 300);
@@ -20,7 +33,7 @@ function draw() {
 }
 
 //Create a custom shape. Use input parameters for position and size
-function customShape(x, y, size){
+function customShape circlez(x, y, size){
   //Outside circle - thin line, with a blue-ish fill
   stroke(134, 151, 178);
   strokeWeight(1);
@@ -47,4 +60,106 @@ function customShape(x, y, size){
   noStroke();
   fill(196, 204, 171);
   ellipse(x, y, size/4, size/4);
+}
+let squarez;
+let xRandom;
+let yRandom;
+let sizeRandom = 100;
+
+function setup() {
+  createCanvas(500, 500);
+  frameRate(10); //Drawing speed 10 fps
+  noStroke();
+}
+
+function draw() {
+  //Randomize the variables for the position and size
+  xRandom = random (width);
+  yRandom = random (height);
+  sizeRandom = random(100, 300);
+
+  //Call the function. Pass the arguments for position and size
+  customShape(xRandom, yRandom, sizeRandom);
+}
+
+//Create a custom shape. Use input parameters for position and size
+function customShape squarez(x, y, size){
+  //Outside circle - thin line, with a blue-ish fill
+  stroke(134, 151, 178);
+  strokeWeight(1);
+  fill(random(255),random(255),random(255));
+  rect(x, y, size, size);
+
+  //Next circle is smaller, with thicker stroke with a pink-ish color
+  strokeWeight(5);
+  stroke(random(255),random(255),random(255));
+  noFill();
+  rect(x/2, y/2, size/1.2, size/1.2);
+
+  //Next circle is smaller, with thin stroke and gray-ish color
+  strokeWeight(1);
+  stroke(random(255),random(255),random(255));
+  rect(x/4, y/4, size/1.4, size/1.4);
+
+  //Next circle is smaller, with thicker stroke with a blue-grey color
+  stroke(random(255),random(255),random(255));
+  strokeWeight(5);
+  rect(x/8, y/8, size/2, size/2);
+
+  //Next circle is smaller, with a green-ish fill, and no stroke
+  noStroke();
+  fill(random(255),random(255),random(255));
+  triangle(x/16, y/16, size/4, size/4);
+}
+//Create three global scope variables for position and size
+let trigl;
+let xRandom;
+let yRandom;
+let sizeRandom = 100;
+
+function setup() {
+  createCanvas(500, 500);
+  frameRate(10); //Drawing speed 10 fps
+  noStroke();
+}
+
+function draw() {
+  //Randomize the variables for the position and size
+  xRandom = random (width);
+  yRandom = random (height);
+  sizeRandom = random(100, 300);
+
+  //Call the function. Pass the arguments for position and size
+  customShape(xRandom, yRandom, sizeRandom);
+}
+
+//Create a custom shape. Use input parameters for position and size
+function customShape trigl(x, y, size){
+  //Outside circle - thin line, with a blue-ish fill
+  stroke(random(255),random(255),random(255));
+  strokeWeight(1);
+  fill(random(255),random(255),random(255));
+  triangle(x, y, size, size);
+
+  //Next circle is smaller, with thicker stroke with a pink-ish color
+  strokeWeight(5);
+  stroke(random(255),random(255),random(255));
+  fill(random(255),random(255),random(255));;
+  triangle(x, y, size/1.2, size/1.2);
+
+  //Next circle is smaller, with thin stroke and gray-ish color
+  strokeWeight(1);
+  stroke(random(255),random(255),random(255));
+  triangle(x, y, size/1.4, size/1.4);
+
+  //Next circle is smaller, with thicker stroke with a blue-grey color
+  noStroke();
+  strokeWeight(5);
+  fill(random(255),random(255),random(255));
+  triangle(x, y, size/2, size/2);
+
+  //Next circle is smaller, with a green-ish fill, and no stroke
+  noStroke();
+  fill(random(255),random(255),random(255));
+  triangle(x, y, size/4, size/4);
 }
