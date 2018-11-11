883ff117dedad29c618d913cea3d80046c845d3f
becky giffen <rebecca.giffen@students.plymouth.ac.uk>
2018-11-11 20:29:44 +0000

    cv

--- Homework 2/sketch.js
+++ Homework 2/sketch.js
@@ -1,40 +1,57 @@
-//DAT405 / GAD405
-//04_Exercise01 - Loops and Random Color
-
-let size = 100;
-let backImg;
-let xPlusOne = 0;
-let yPlusOne = 0;
+//RANDOMCIRCLESBABES
+let size;
+let x;
+let y;
 
 function setup() {
   createCanvas(500, 500);
-  frameRate(3); //Drawing speed 3 fps
-  backImg = loadImage("patternXY.png");
-  backImg.resize(500,500);
+  frameRate(15);
+  noStroke();
+
 }
 
 function draw() {
-  background(backImg);
-  rects(xPlusOne, yPlusOne);
-
   //Run the nested loop for x, and y
-  //for (let y = 0; y < 5; y++) {
-    //for (let x = 0; x < 5; x++) {
-      //rects (Math.round(random(4)), Math.round(random(4)));
-    //}
-  //}
-}
+size = random(100,100);
+x = random (width);
+y = random (height);
+customShape(x,y,size);
 
-function rects(x, y){
-  //Background rect()
-  fill(215, 200, 170);
-  rect(size * x, size * y, size, size);
+    }
 
-  //Draw the foreground shapes with a random RGB color
-  fill(random(255), random(255), random(255));
-  //Create a slight offeset for the x and y, to appear in the middle
-  rect(25+size * x, 25+size * y, size/2, size/2);
-}
+
+function customShape(x,y,size) {
+
+stroke(0);
+strokeWeight(1);
+noFill();
+ellipse(x,y,size,size);
+
+strokeWeight(5);
+fill('green');
+ellipse(x,y,size/2,size/2);
+
+strokeWeight(3);
+fill('blue');
+rect(x,y,size/4, size/4);
+
+strokeWeight(2);
+triangle(x,y,size/8, size/8);
+
+strokeWeight(1);
+fill('yellow');
+ellipse(x,y,size/10, size/10);
+
+strokeWeight(1);
+fill('white');
+rect(x,y,size/12, size/12);
+
+
+
+
+
+
+    }
 
 function keyPressed() {
   if (key == 'd' || key == 'D') xPlusOne++;
