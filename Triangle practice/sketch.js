a57c6b4850a7890deac182548372edbe0ff27172
becky giffen <rebecca.giffen@students.plymouth.ac.uk>
2018-11-11 20:56:34 +0000

    zczc

--- Triangle practice/sketch.js
+++ Triangle practice/sketch.js
@@ -1,54 +1,50 @@
-//RANDOMCIRCLESBABES
-let size;
-let x;
-let y;
+//Create three global scope variables for position and size
+let xRandom;
+let yRandom;
+let sizeRandom = 100;
 
 function setup() {
   createCanvas(500, 500);
-  frameRate(15);
+  frameRate(10); //Drawing speed 10 fps
   noStroke();
-
 }
 
 function draw() {
-  //Run the nested loop for x, and y
-size = random(100,100);
-x = random (width);
-y = random (height);
-customShape(x,y,size);
+  //Randomize the variables for the position and size
+  xRandom = random (width);
+  yRandom = random (height);
+  sizeRandom = random(100, 300);
 
-    }
+  //Call the function. Pass the arguments for position and size
+  customShape(xRandom, yRandom, sizeRandom);
+}
 
+//Create a custom shape. Use input parameters for position and size
+function customShape(x, y, size){
+  //Outside circle - thin line, with a blue-ish fill
+  stroke(134, 151, 178);
+  strokeWeight(1);
+  fill(239, 245, 255);
+  ellipse(x, y, size, size);
 
-function customShape(x,y,size) {
+  //Next circle is smaller, with thicker stroke with a pink-ish color
+  strokeWeight(5);
+  stroke(255, 207, 200);
+  noFill();
+  ellipse(x, y, size/1.2, size/1.2);
 
-stroke(0);
-strokeWeight(1);
-noFill();
-ellipse(x,y,size,size);
+  //Next circle is smaller, with thin stroke and gray-ish color
+  strokeWeight(1);
+  stroke(177, 178, 174);
+  ellipse(x, y, size/1.4, size/1.4);
 
-strokeWeight(5);
-fill('green');
-ellipse(x,y,size/2,size/2);
+  //Next circle is smaller, with thicker stroke with a blue-grey color
+  stroke(101, 141, 166);
+  strokeWeight(5);
+  ellipse(x, y, size/2, size/2);
 
-strokeWeight(3);
-fill('blue');
-rect(x,y,size/4, size/4);
-
-strokeWeight(2);
-triangle(x,y,size/8, size/8);
-
-strokeWeight(1);
-fill('yellow');
-ellipse(x,y,size/10, size/10);
-
-strokeWeight(1);
-fill('white');
-rect(x,y,size/12, size/12);
-
-
-
-
-
-
-    }
+  //Next circle is smaller, with a green-ish fill, and no stroke
+  noStroke();
+  fill(196, 204, 171);
+  ellipse(x, y, size/4, size/4);
+}
