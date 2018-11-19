d16f7d0161f3a8cd12f43fa99aecc7747f0d218c
becky giffen <rebecca.giffen@students.plymouth.ac.uk>
2018-11-19 22:43:17 +0000

    x

--- courseworkpractice/sketch.js
+++ courseworkpractice/sketch.js
@@ -1,36 +1,60 @@
-//Loops
+//virtual lego
 var x;
 var y;
 var squaresize = 50; //for every squaresize written, we know it is a 50 square
-//var mouse1;
-/*
-var coloursquare; //1=blue,2=green,3=yellow,4=red,5=orange,6=violet,7=pink
-var mousepos1;
-var mousepos2;
-*/
+//var coloursquare; //0=blue,1=green,2=yellow,4=red,5=orange,6=violet,7=pink
+
+
 
 function setup(){
   createCanvas (600,600); //size of canvas
  stroke(2,2,2); //black lines on grid
+ rectMode(CENTER);
+ for (y = 2; y < 22; y++){ //y coord 20 squares
+   for (x = 2; x < 22; x++){ //x coord 20 squares
+     var mousepos1 = map(mouseX,0,width,0,100); //MOUSEPOSITION BETWEEN THESE POINTS
+    var mousepos2 = map(mouseX,0,width,0,100);
+     fill('white');
+      rect(squaresize*x, squaresize*y, squaresize,squaresize);//x 0 x50 etc
 }
+}
+}
+
+
+/*
 function keyPressed() {
-/*  if (key == 'b' || key == 'B') coloursquare=1; loop();//circle
-  if (key == 'g' || key == 'G') coloursquare=2; loop(); //triangle
-  if (key == 'y' || key == 'Y') coloursquare=3; loop();//squares
-  if (key =='r' || key =='R') coloursquare=4;
-  if (key =='o'|| key =='O')coloursquare=5;
-  if (key== 'v'|| key =='V') coloursquare=6;
-  if (key== 'p'|| key =='P')coloursquare=7;
+  if (key == 'b' || key == 'B') coloursquare=0;
+  if (key == 'g' || key == 'G') coloursquare=1; loop();
+  if (key == 'y' || key == 'Y') coloursquare=2; loop();
+  if (key =='r' || key =='R') coloursquare=3;
+  if (key =='o'|| key =='O')coloursquare=4;
+  if (key== 'v'|| key =='V') coloursquare=5;
+  if (key== 'p'|| key =='P')coloursquare=6;
 */
+
+
+
+
+
   function draw(){
-    //Run the nested loop for x, and y
-    for (var y = 2; y < 22; y++){ //y coord 20 squares
+    //Run the nested loop for x, and y, THIS IS TO DRAW THE GRID
+  /*  for (var y = 2; y < 22; y++){ //y coord 20 squares
       for (var x = 2; x < 22; x++){ //x coord 20 squares
-        var mousepos = map(mouseX,2,width,2,102); //MOUSEPOSITION BETWEEN THESE POINTS
+        var mousepos1 = map(mouseX,2,width,2,102); //MOUSEPOSITION BETWEEN THESE POINTS
        var mousepos2 = map(mouseX,2,width,2,102);
         fill('white');
          rect(squaresize*x, squaresize*y, squaresize,squaresize);//x 0 x50 etc
+         */
+
+        //   fill('blue');
+          // rect(mouseX,mouseY,squaresize,squaresize);
+
+
 //
 }
-}
+function mouseClicked(){
+/*if (mouseX(2<102) && mouseY(2<102));*/
+
+fill ('blue');
+rect(mouseX,mouseY,squaresize-1,squaresize-1);
 }
---
+++ practice squares/sketch.js
@@ -0,0 +1,34 @@
+var newcol= 0;
+var x;
+var y;
+var sqsz = 40;
+//var coords[100]= x0 < x99, y0 <y99;
+
+//var clicker;
+
+
+function setup(){
+createCanvas(400,400);
+}
+
+function draw() {
+//  fill(newcol);
+
+  for (let y=0; y<10; y++){
+   for (let x =0; x<10; x++){
+     stroke('0');
+     fill(newcol);
+     rect(sqsz*x, sqsz*y,sqsz,sqsz);//x 0 x50 etc
+   }
+
+}
+}
+
+function mousePressed() {
+  if(mouseX,mouseY,sqsz,sqsz,newcol ==100) {
+  rect(mouseX,mouseY,sqsz,sqsz);
+  } else {
+    newcol = 0;
+
+}
+}
