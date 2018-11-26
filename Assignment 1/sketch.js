1250b5cb46729c3204a9b762f6b72a13f5de42c7
becky giffen <rebecca.giffen@students.plymouth.ac.uk>
2018-11-26 08:55:08 +0000

    herrroo

--- Assignment 1/sketch.js
+++ Assignment 1/sketch.js
@@ -4,22 +4,30 @@ var alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q
 //this is my array to choose the random letter from
 var counter = 0; //Counting in seconds
 var countdown = 10; //Counting down variable, start at 10
-
+//var interval = setInterval();
 function setup(){
   createCanvas(594,841); //size of the frame
   frameRate(5);//frame rate
   var timer=select('#timer'); //using a timer which is put into the index
   timer.html(countdown-counter); //starting from 10,going down 1 second at a time to 0
-  var interval = setInterval(timeIt, 1000); //1000 is the amount of miliseconds in 1 sec
+  var interval =  setInterval(timeIt, 1000); //1000 is the amount of miliseconds in 1 sec
 function timeIt(){ //timing function
   counter++; //counting a second at a time
   timer.html(countdown-counter); //counting down from 10, by 1 each time
-  if (counter ==countdown){
-  //once we get to zero the timer stops
-  clearInterval(interval);
+  if (counter ==countdown){//once we get to zero the timer stops
+
+  clearInterval(interval);//stops timer
   }
   }
 }
+window.onload = setupRefresh;
+
+function setupRefresh() { //
+      setTimeout("refreshPage();", 10000); // when timer gets to 10000miliseconds
+    }                                    //the page refreshes
+    function refreshPage() {
+       window.location = location.href; //refreshes the window
+    }
 
 function draw(){ //drawing our programme
 
@@ -32,7 +40,6 @@ function draw(){ //drawing our programme
   ellipse(280,280,500,500); //large circle stays the same size and same position constantly
   stroke(random(255),random(255),random(255));
   var shape1=ellipse(mouseX,mouseY,random(200),random(200)); //first shape is a circle at the point of the mouse position, random size
-
   var shape2=rect(mouseX,mouseY,50,50);//second variable shape is a square, which appears on mouse position size 50,50
 }
 
